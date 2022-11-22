2c863d443c5525fbfb591bc0e6e44fba8d3be5d2
ranisantos <raniel.santos@mercadolivre.com>
2022-11-22 19:14:51 -0300

    Adiciona coleçao do postman e retira a chave da api que estava a mostra

--- src/main/java/com/bootcamp/melifrescos/util/GeoApiContext.java
+++ src/main/java/com/bootcamp/melifrescos/util/GeoApiContext.java
@@ -1,9 +1,11 @@
 package com.bootcamp.melifrescos.util;
 
+import org.springframework.context.expression.EnvironmentAccessor;
+
 public class GeoApiContext {
 
     private final static com.google.maps.GeoApiContext context = new com.google.maps.GeoApiContext.Builder()
-            .apiKey("AIzaSyCTxzdD9SK7KvrwvTK1nt1n9HGT0-E91-I")
+            .apiKey("GOOGLE-API-KEY")
             .build();
 
     public static com.google.maps.GeoApiContext getContext() {
