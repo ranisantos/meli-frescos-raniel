b45969c71ec57f0e1a11c7dbfe19487ce9d2a620
ranisantos <raniel.santos@mercadolivre.com>
2022-11-23 07:31:06 -0300

    Adiciona coleçao do postman atualizada

--- src/main/java/com/bootcamp/melifrescos/util/GeoApiContext.java
+++ src/main/java/com/bootcamp/melifrescos/util/GeoApiContext.java
@@ -5,7 +5,7 @@ import org.springframework.context.expression.EnvironmentAccessor;
 public class GeoApiContext {
 
     private final static com.google.maps.GeoApiContext context = new com.google.maps.GeoApiContext.Builder()
-            .apiKey("GOOGLE-API-KEY")
+            .apiKey("AIzaSyCTxzdD9SK7KvrwvTK1nt1n9HGT0-E91-I")
             .build();
 
     public static com.google.maps.GeoApiContext getContext() {
